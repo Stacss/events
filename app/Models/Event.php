866d7864f06d89c3fe7677d5076dec8e43d8c0e7
Stas <postnikov.sa@ya.ru>
2023-12-18 17:53:37 +0700

    create event model

---
+++ app/Models/Event.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    /**
+     * Атрибуты, которые можно массово присваивать.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'title',
+        'text',
+        'creator_id',
+    ];
+
+    /**
+     * Отношение к создателю события.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    /**
+     * Отношение ко всем участникам события.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function participants()
+    {
+        return $this->belongsToMany(User::class, 'event_user', 'event_id', 'user_id');
+    }
+}
