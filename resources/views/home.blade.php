dfffc2a8da1dd325bf0d21947bca5a6ec6407737
Stas <postnikov.sa@ya.ru>
2023-12-20 14:41:47 +0700

    Admin lte Auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+
+    <div class="row justify-content-center pt-3">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -15,9 +15,11 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+
                 </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
